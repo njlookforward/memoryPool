7e24593e983812596706505e2a7b4892914ec77f
nj22 <njlookforward@163.com>
2025-05-01 11:09:48 +0800

    complete atomic operation/lock-free queue to improve _freeList and pushFreeList and popFreeList on branch feature-atomic

--- v1.0/include/memoryPool.h
+++ v1.0/include/memoryPool.h
@@ -1,28 +1,31 @@
 #ifndef V_1_0_MEMORYPOOL_H
 #define V_1_0_MEMORYPOOL_H
 
-/// @warning origin three define in the namespace memoryPool
-#define HASH_BACKET_SIZE 64
-#define SLOT_BASE_SIZE 8
-#define SLOT_MAX_SIZE 512
-
 // 大胆尝试，大胆实践，不要怂，错了改过去不就好了，我就是一名工程师，我要一小块一小块的敲掉他
 // 就算做出一堆狗屎又如何，我要做出来
 
 #include <vector>
 #include <mutex>
+#include <atomic>
 #include <new>  // use operator new and operator delete
 #include <cstddef>
 #include <cstdint>
 #include <utility>
 #include <stdexcept>
+#include <cassert>
 
 // 在大型项目中，必须使用namespace进行命名空间的分割
 namespace memoryPool {
 
+/// @warning origin three define in the namespace memoryPool
+/// @remark 这种预编译量定义在命名空间中
+#define HASH_BACKET_SIZE 64
+#define SLOT_BASE_SIZE 8
+#define SLOT_MAX_SIZE 512
+
 /// @param Slot代表内存池中的每一个内存块，同时充分地利用内存块，记录信息，目前只包含下一个空闲的内存块地址
 struct Slot {
-    Slot *_next;    // 记录下一个空闲的狭槽在哪里
+    std::atomic<Slot *> _next;    // 记录下一个空闲的狭槽在哪里
 };
 
 /// @brief memoryPool是每一个内存池管理员，记录这个内存池的关键信息，并真正负责内存池中每一个slot的分配与回收
@@ -38,24 +41,23 @@ public:
 
 /// @remarks 哪些是临界区，需要使用互斥量进行同步，这需要我自己慢慢体会
 /// @public init() 用来确定Slot的大小
-    void init(int Slotsize);
+    void init(std::size_t Slotsize);
     void *allocate();
-    void deallocate(Slot *);
+    void deallocate(void *);
 
 private:
     void newBlock();
-    int paddingBlock(Slot *, int); // 进行内存对齐
+    std::size_t paddingBlock(Slot *, std::size_t); // 进行内存对齐
     void *popFreeList();
-    void pushFreeList(Slot *);
+    void pushFreeList(void *);
 
 private:
-    int _blockSize = 4096;            // 每个内存池真正的整块内存是固定的4KB
-    int _slotSize = 0;                // 待分配的每一个小内存块的大小
+    std::size_t _blockSize = 4096;            // 每个内存池真正的整块内存是固定的4KB
+    std::size_t _slotSize = 0;                // 待分配的每一个小内存块的大小
     Slot *_firstBlock = nullptr;      // 可用的内存块地址
-    Slot *_freeList = nullptr;        // 指向空闲链表的表头
+    std::atomic<Slot *> _freeList;        // 指向空闲链表的表头
     Slot *_curSlot = nullptr;         // 指向目前的第一个unused Slot
     Slot *_lastSlot = nullptr;        // 代表该memoryPool的最后一个内存块的位置标识
-    std::mutex _mutexForFreeList;     // 该互斥量用于操作_freeList
     std::mutex _mutexForBlock;        // 该互斥量用于操作block
 };
 
--- v1.0/src/memoryPool.cpp
+++ v1.0/src/memoryPool.cpp
@@ -2,8 +2,13 @@
 #include "memoryPool.h"
 using std::lock_guard;
 using std::mutex;
+using std::memory_order_relaxed;
+using std::memory_order_release;
+using std::memory_order_acquire;
+
 using std::uintptr_t;
 using std::size_t;
+
 using std::vector;
 using std::runtime_error;
 namespace memoryPool {
@@ -23,8 +28,9 @@ MemoryPool::~MemoryPool() {
 }
 
 /// @brief 该函数仅是指定狭槽的大小，只有需要进行allocate的时候才会真正的申请一整块大内存
-void MemoryPool::init(int SlotSize) {
+void MemoryPool::init(size_t SlotSize) {
     _slotSize = SlotSize;
+    _freeList.store(nullptr, memory_order_relaxed);
 }
 
 void *MemoryPool::allocate() {
@@ -34,7 +40,6 @@ void *MemoryPool::allocate() {
 /// 在示例文件中，尽管已经判断过_freeList是否为空，但是因为是多线程的环境，可能别的线程已经加锁导致为空
 /// 因此需要再次进行判断_freeList是否为空
     {
-        lock_guard<mutex> freeListLock(_mutexForFreeList);
         void *newSlot = popFreeList();
         if(newSlot != nullptr)
             return newSlot;
@@ -49,11 +54,9 @@ void *MemoryPool::allocate() {
     return reinterpret_cast<void*>(newSlot);
 }
 
-void MemoryPool::deallocate(Slot *freeSlot) {
+void MemoryPool::deallocate(void *freeSlot) {
 /// @brief _freeList是临界资源，需要进行同步操作
-    lock_guard<mutex> freeListLock(_mutexForFreeList);
-    freeSlot->_next = _freeList;
-    _freeList = freeSlot;
+    pushFreeList(freeSlot);
 }
 
 /// @attention 是不是需要将加锁操作放在实现函数中，这样同步意图更加明显，更加具体
@@ -61,7 +64,7 @@ void MemoryPool::newBlock() {
 /// @brief 因为是具体的实现函数，而加锁操作已经在接口函数完成了，所以仍然保持同步
     Slot *newblock = reinterpret_cast<Slot*>(operator new(_blockSize));
     // 接下来要充分的利用这一大块内存
-    newblock->_next = _firstBlock;
+    newblock->_next.store(_firstBlock, memory_order_relaxed);
     _firstBlock = newblock;
     // 接下来需要重设_curSlot and _lastSlot
     // 但在此之前，需要进行对齐操作
@@ -71,23 +74,39 @@ void MemoryPool::newBlock() {
     _lastSlot = reinterpret_cast<Slot *>(reinterpret_cast<char *>(_firstBlock) + _blockSize - _slotSize + 1);
 }
 
-int MemoryPool::paddingBlock(Slot *slot, int align) {
+size_t MemoryPool::paddingBlock(Slot *slot, size_t align) {
 /// @remark 有优化的空间，可以使用位运算比模运算快10倍
     return (align - (reinterpret_cast<uintptr_t>(slot) % align)) % align;
 }
 
-void *MemoryPool::popFreeList() {
-/// @brief 先从空闲链表中分配slot，然后更新_freeList，同样属于实现函数，加锁操作已经在接口函数中完成了
-    if(_freeList == nullptr)
-        return nullptr;
-    Slot *curFreeSlot = _freeList;
-    _freeList = curFreeSlot->_next;
-    return reinterpret_cast<void*>(curFreeSlot);
+/**
+ * @brief 对于临界资源_freeList的读写操作仍然是采用无锁队列，被atomic所包装的指针操作，都是使用成员函数，而不是其他的
+*/
+void MemoryPool::pushFreeList(void *ptr) {
+    Slot *slot = reinterpret_cast<Slot *>(ptr);
+    while (true)
+    {
+        Slot *oldHead = _freeList.load(memory_order_relaxed);
+        slot->_next.store(oldHead, memory_order_relaxed);
+        if(_freeList.compare_exchange_weak(oldHead, slot, memory_order_release, memory_order_relaxed))
+            return;    
+    }
 }
 
-void MemoryPool::pushFreeList(Slot *slot) {
-    slot->_next = _freeList;
-    _freeList = slot;
+/// @brief 先从空闲链表中分配slot，然后更新_freeList，同样属于实现函数，加锁操作已经在接口函数中完成了
+/// @details 因为对于小内存对象的分配和回收是非常频繁的操作，因此优化方法是使用无锁队列取代互斥锁来进行加速
+/// @attention 具体的并发操作我并没有理解透彻，先接受这样操作，为什么在popFreeList中使用memory_order_acquire，而不是memory_order_acq_rel??
+void *MemoryPool::popFreeList() {
+    while (true)
+    {
+        Slot *oldHead = _freeList.load(memory_order_relaxed);
+        if(oldHead == nullptr)
+            return nullptr;
+        
+        Slot *newHead = oldHead->_next.load(memory_order_relaxed);
+        if(_freeList.compare_exchange_weak(oldHead, newHead, memory_order_acquire, memory_order_relaxed))
+            return oldHead;
+    }
 }
 
 vector<MemoryPool> HashBucket::_memoryPoolSet(HASH_BACKET_SIZE);
